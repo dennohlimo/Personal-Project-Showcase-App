08b4b6b1fe49dd25bc7dab49275e4b9a328f27db
Dennis Kipruto Sambu <dennoh86@gmail.com>
2026-09-06 01:28:14 +0300

    Feature to display products on the shop page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Routes } from "react-router";
 import Layout from "./layouts/Layout";
 import Home from "./components/Home";
 import AdminPortal from "./components/AdminPortal";
 import Shop from "./components/Shop";
-import PastryDetail from "./pages/PastryDetail";
+import PastryDetail from "./components/PastryDetail";
 
 function App() {
+  const [pastries, setPastries] = useState([]);
+  const url = "http://localhost:3000/pastries";
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPastries(data))
+      .catch((err) => console.error("Something went wrong", err));
+  }, []);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop" element={<Shop pastries={pastries} />} />
         <Route path="shop/:id" element={<PastryDetail />} />
         <Route path="admin" element={<AdminPortal />} />
       </Route>
